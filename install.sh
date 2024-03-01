db2eb69784784dfdc4e2de9bb60e7e399402d438
Felipe Chierice <hello@felipechierice.dev>
2024-03-01 14:48:10 -0300

    feat: genesis

---
+++ install.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+# Default installation path
+default_install_path="$HOME/.local/bin/lst"
+
+# Parse command-line arguments for custom install path or silent mode
+for arg in "$@"
+do
+    case $arg in
+        --silent)
+        silent="true"
+        shift # Remove --silent from processing
+        ;;
+        --path=*)
+        custom_install_path="${arg#*=}"
+        shift # Remove --path=value from processing
+        ;;
+        --backup)
+        backup="true"
+        shift # Remove --backup from processing
+        ;;
+    esac
+done
+
+install_path="${custom_install_path:-$default_install_path}"
+
+# Function to echo only if not in silent mode
+echo_if_not_silent() {
+    if [ -z "$silent" ]; then
+        echo "$@"
+    fi
+}
+
+# Check for dependencies (example)
+check_dependencies() {
+    missing_deps=0
+    command -v curl >/dev/null 2>&1 || { echo_if_not_silent "curl is not installed. Please install it and try again."; let missing_deps++; }
+    command -v python3 >/dev/null 2>&1 || { echo_if_not_silent "python3 is not installed. Please install it and try again."; let missing_deps++; }
+    if [ $missing_deps -ne 0 ]; then
+        exit 1
+    fi
+}
+
+# Create virtual environment and install requirements
+create_virtualenv_and_install_requirements() {
+    local venv_path="$install_path/venv"
+    echo_if_not_silent "Creating virtual environment in $venv_path."
+    
+    # Try using virtualenv first
+    if command -v virtualenv >/dev/null 2>&1; then
+        virtualenv "$venv_path"
+    # Fallback to python -m venv
+    elif command -v python3 >/dev/null 2>&1; then
+        python3 -m venv "$venv_path"
+    else
+        echo_if_not_silent "Neither virtualenv nor python3 venv module is available. Please install one of them and try again."
+        exit 1
+    fi
+
+    source "$venv_path/bin/activate"
+    if [ -f "$(dirname "$0")/requirements.txt" ]; then
+        pip install -r "$(dirname "$0")/requirements.txt"
+        echo_if_not_silent "Requirements installed successfully."
+    else
+        echo_if_not_silent "No requirements.txt found. Skipping dependency installation."
+    fi
+    deactivate
+}
+
+# Backup existing installation
+backup_if_needed() {
+    if [ "$backup" == "true" ] && [ -d "$install_path" ]; then
+        local timestamp=$(date +%Y%m%d-%H%M%S)
+        local backup_path="${install_path}_backup_$timestamp"
+        mv "$install_path" "$backup_path"
+        echo_if_not_silent "Existing installation backed up to '$backup_path'"
+    fi
+}
+
+# Installation steps
+perform_installation() {
+    echo_if_not_silent "Attempting to install lst at '$install_path'."
+    mkdir -p "$install_path"
+    # Assuming the script's path is here
+    cp  "$(dirname "$0")/main.py" "$install_path/main.py"
+    cp  "$(dirname "$0")/lst" "$install_path/lst"
+    chmod +x "$install_path/lst"
+    echo_if_not_silent "Installation completed."
+}
+
+# Update PATH
+update_path_in_shell() {
+    local shell_config_file
+    case "$SHELL" in
+    */bash)
+        shell_config_file="$HOME/.bashrc"
+        ;;
+    */zsh)
+        shell_config_file="$HOME/.zshrc"
+        ;;
+    */fish)
+        shell_config_file="$HOME/.config/fish/config.fish"
+        ;;
+    *)
+        echo_if_not_silent "Unsupported shell for automatic PATH update. Please manually add $install_path to your PATH."
+        return
+        ;;
+    esac
+
+    if ! grep -q "$install_path" "$shell_config_file" ; then
+        echo "export PATH=\"$install_path:\$PATH\"" >> "$shell_config_file"
+        echo_if_not_silent "Added $install_path to PATH in $shell_config_file."
+    else
+        echo_if_not_silent "$install_path is already in your PATH."
+    fi
+}
+
+# Check if already installed and prompt for reinstallation
+check_existing_installation() {
+    if [ -d "$install_path" ]; then
+        if [ -z "$silent" ]; then
+            read -p "lst is already installed. Do you want to proceed with reinstallation? (y/n) " -n 1 -r
+            echo
+            if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+                echo "Installation cancelled."
+                exit 0
+            fi
+        fi
+        backup_if_needed
+    fi
+}
+
+main() {
+    check_dependencies
+    check_existing_installation
+    create_virtualenv_and_install_requirements
+    perform_installation
+    # Detect shell and ask user for input
+    echo "You are using the $SHELL shell."
+    read -p "Do you want to automatically add $HOME/.local/bin to your PATH? (y/n) " -n 1 -r
+    echo # move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        update_path_in_shell
+    else
+        echo "Please add $HOME/.local/bin to your PATH manually."
+    fi
+    echo_if_not_silent "Installation completed successfully."
+    echo_if_not_silent "Please restart your shell to start using lst."
+}
+
+main "$@"
---
+++ main.py
@@ -0,0 +1,514 @@
+import stat
+import os
+import argparse
+from rich.console import Console
+from rich.table import Table
+from rich.progress import Progress, TaskID
+from datetime import datetime, timedelta
+import re
+from pwd import getpwuid
+from grp import getgrgid
+
+extensions = {
+    "mp3": " ",
+    "wav": " ",
+    "m4a": " ",
+    "ogg": " ",
+    "flac": " ",
+    "aac": " ",
+    "wma": " ",
+    "aiff": " ",
+    "alac": " ",
+    "ape": " ",
+    "opus": " ",
+    "pcm": " ",
+    "ac3": " ",
+    "amr": " ",
+    "mid": " ",
+    "mp4": " ",
+    "avi": " ",
+    "mkv": " ",
+    "mov": " ",
+    "wmv": " ",
+    "flv": " ",
+    "webm": " ",
+    "m4v": " ",
+    "mpeg": " ",
+    "3gp": " ",
+    "ts": " ",
+    "vob": " ",
+    "rm": " ",
+    "rmvb": " ",
+    "ogv": " ",
+    "styl": " ",
+    "sass": " ",
+    "scss": " ",
+    "htm": " ",
+    "html": " ",
+    "slim": " ",
+    "ejs": " ",
+    "css": " ",
+    "less": " ",
+    "json": " ",
+    "js": " ",
+    "mjs": " ",
+    "jsx": " ",
+    "rb": " ",
+    "php": " ",
+    "py": " ",
+    "pyc": " ",
+    "pyo": " ",
+    "pyd": " ",
+    "coffee": " ",
+    "mustache": " ",
+    "hbs": " ",
+    "conf": " ",
+    "ini": " ",
+    "yml": " ",
+    "yaml": " ",
+    "toml": " ",
+    "bat": " ",
+    "jpg": " ",
+    "jpeg": " ",
+    "bmp": " ",
+    "png": " ",
+    "webp": " ",
+    "gif": " ",
+    "ico": " ",
+    "twig": " ",
+    "cpp": " ",
+    "c++": " ",
+    "cxx": " ",
+    "cc": " ",
+    "cp": " ",
+    "c": " ",
+    "cs": " ",
+    "h": " ",
+    "hh": " ",
+    "hpp": " ",
+    "hxx": " ",
+    "hs": " ",
+    "lhs": " ",
+    "lua": " ",
+    "java": " ",
+    "sh": " ",
+    "fish": " ",
+    "bash": " ",
+    "zsh": " ",
+    "ksh": " ",
+    "csh": " ",
+    "awk": " ",
+    "ps1": " ",
+    "ml": "λ ",
+    "mli": "λ ",
+    "diff": " ",
+    "db": " ",
+    "sql": " ",
+    "dump": " ",
+    "clj": " ",
+    "cljc": " ",
+    "cljs": " ",
+    "edn": " ",
+    "scala": " ",
+    "go": " ",
+    "dart": " ",
+    "xul": " ",
+    "sln": " ",
+    "suo": " ",
+    "pl": " ",
+    "pm": " ",
+    "t": " ",
+    "rss": " ",
+    "f#": " ",
+    "fsscript": " ",
+    "fsx": " ",
+    "fs": " ",
+    "fsi": " ",
+    "rs": " ",
+    "rlib": " ",
+    "d": " ",
+    "erl": " ",
+    "hrl": " ",
+    "ex": " ",
+    "exs": " ",
+    "eex": " ",
+    "leex": " ",
+    "vim": " ",
+    "ai": " ",
+    "psd": " ",
+    "psb": " ",
+    "ts": " ",
+    "tsx": " ",
+    "jl": " ",
+    "pp": " ",
+    "vue": "﵂ ",
+    "elm": " ",
+    "swift": " ",
+    "xcplayground": " ",
+    "txt": " ",
+    "log": " ",
+    "md": " ",
+    "mdx": " ",
+    "markdown": " ",
+    "rmd": " ",
+    "rst": " ",
+    "tex": " ",
+    "texi": " ",
+    "texinfo": " ",
+    "zip": " ",
+    "tar": " ",
+    "gz": " ",
+    "bz2": " ",
+    "xz": " ",
+    "7z": " ",
+    "rar": " "
+}
+
+patterns = {
+    ".*jquery.*.js$": " ",
+    ".*angular.*.js$": " ",
+    ".*backbone.*.js$": " ",
+    ".*require.*.js$": " ",
+    ".*materialize.*.js$": " ",
+    ".*materialize.*.css$": " ",
+    ".*mootools.*.js$": " ",
+    ".*\.lock$": "󱧉 ",
+}
+
+exact = {
+    "exact-match-case-sensitive-1.txt": "1",
+    "exact-match-case-sensitive-2": "2",
+    "gruntfile.coffee": " ",
+    "gruntfile.js": " ",
+    "gruntfile.ls": " ",
+    "gulpfile.coffee": " ",
+    "gulpfile.js": " ",
+    "gulpfile.ls": " ",
+    "mix.lock": " ",
+    "dropbox": "  ",
+    ".ds_store": " ",
+    ".gitconfig": " ",
+    ".gitignore": " ",
+    ".gitlab-ci.yml": " ",
+    ".bashrc": " ",
+    ".zshrc": " ",
+    ".vimrc": " ",
+    ".gvimrc": " ",
+    "_vimrc": " ",
+    "_gvimrc": " ",
+    ".bashprofile": " ",
+    "favicon.ico": " ",
+    "license": " ",
+    "node_modules": " ",
+    "react.jsx": " ",
+    "procfile": " ",
+    "dockerfile": " ",
+    "docker-compose.yml": " ",
+    "makefile": " ",
+    "cmakelists.txt": " ",
+}
+
+home_dirs = {
+    "Desktop": " ",
+    "Documents": " ",
+    "Downloads": " ",
+    "Music": " ",
+    "Pictures": " ",
+    "Games": "󱎓 ",
+    "Videos": " ",
+    "Public": " ",
+}
+
+# Initialize a Rich console
+console = Console()
+
+def sizeof_fmt(num, suffix='B'):
+    # Define colors for each unit
+    colors = {
+        '': 'white',  # Bytes
+        'K': 'bold white',  # Kilobytes
+        'M': 'yellow',  # Megabytes
+        'G': 'dim red',  # Gigabytes
+        'T': 'red',  # Terabytes
+        'P': 'red',  # Petabytes
+        'E': 'red',  # Exabytes
+        'Z': 'red',  # Zettabytes
+        'Y': 'red',  # Yottabytes
+    }
+    
+    for unit in colors.keys():
+        if abs(num) < 1024.0:
+            color = colors[unit]
+            return f"[{color}]{num:3.1f} {unit}{suffix}[/]"
+        num /= 1024.0
+    return f"[{colors['Y']}]{num:.1f}Yi{suffix}[/]"
+
+def format_date(timestamp, format='%a %b %d %H:%M:%S %Y'):
+    date = datetime.fromtimestamp(timestamp)
+    date_str = date.strftime(format)
+    today = datetime.now()
+    today_str = today.strftime('%Y-%m-%d')
+    yesterday_str = (today - timedelta(days=1)).strftime('%Y-%m-%d')
+    
+    if date.strftime('%Y-%m-%d') == today_str:
+        return f"[green]{date_str}"
+    elif date.strftime('%Y-%m-%d') == yesterday_str:
+        return f"[dim green]{date_str}"
+    else:
+        return f"[dim white]{date_str}"
+
+def permissions_to_rich_text(permissions):
+    permission_colors = {
+        'd': 'blue',  # Directory
+        'r': 'green',  # Read permission
+        'w': 'yellow',  # Write permission
+        'x': 'red',  # Execute permission
+        'l': 'cyan',  # Symbolic link
+        's': 'bright_red',  # Socket
+        'p': 'bright_yellow',  # Pipe
+        'c': 'bright_green',  # Character device
+        'b': 'bright_blue',  # Block device
+        't': 'bright_magenta',  # Named pipe
+        'u': 'dim white',  # No user
+        '-': 'dim white',  # No permission
+    }
+    rich_text = ''.join([f"[{permission_colors.get(char, 'white')}]{char}[/]" for char in permissions])
+    return rich_text
+
+def get_entry_icon(entry_name, is_directory=False):    
+    if is_directory:
+        default_dir_icon = " "
+        if entry_name in home_dirs: return home_dirs[entry_name]
+        else: return default_dir_icon
+    
+    for key, value in exact.items():
+        if key == entry_name:
+            return value
+
+    for key, value in patterns.items():
+        if re.match(key, entry_name):
+            return value
+        
+    for key, value in extensions.items():
+        entry_ext = entry_name.split('.')[-1] if '.' in entry_name else ''
+        if entry_ext == key:
+            return value
+    
+    return ' '
+
+def calculate_directory_sizes(dir_entries, path, show_all, quiet):
+    directory_sizes = {}
+
+    if not quiet:
+        progress = Progress(console=console, transient=True)
+        with progress:
+            task_id = progress.add_task("[green]Calculating sizes...", total=len(dir_entries), visible=True)
+            for dir_entry in dir_entries:
+                dir_path = os.path.join(path, dir_entry)
+                progress.update(task_id, description=f"[green]Calculating size of {dir_entry}...", advance=1)
+                directory_sizes[dir_entry] = get_dir_size(dir_path, show_all, progress, task_id, quiet)
+
+    else:
+        for dir_entry in dir_entries:
+            dir_path = os.path.join(path, dir_entry)
+            directory_sizes[dir_entry] = get_dir_size(dir_path, show_all, None, None, quiet)
+
+    return directory_sizes
+
+def get_dir_size(dir_path, show_all, progress, task_id, quiet):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        if not show_all:
+            filenames = [f for f in filenames if not f.startswith('.')]
+            dirnames[:] = [d for d in dirnames if not d.startswith('.')]
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            if os.path.exists(fp):
+                total_size += os.path.getsize(fp)
+    return total_size
+
+def get_row_data(entry, path, options, directory_sizes=None):
+    entry_path = os.path.join(path, entry)
+    is_dir = os.path.isdir(entry_path)
+    try:
+        stat_info = os.lstat(entry_path)
+        permissions = permissions_to_rich_text(stat.filemode(stat_info.st_mode))
+        links = str(stat_info.st_nlink) if options['show_links'] else ''
+        
+        # Handling size for directories and files differently
+        size = '[dim white]-[/]'
+        if is_dir and options['show_folder_size']:
+            size = sizeof_fmt(directory_sizes.get(entry, 0))
+        elif not is_dir:
+            size = sizeof_fmt(stat_info.st_size)
+        
+        owner = getpwuid(stat_info.st_uid).pw_name if options['show_owner'] else ''
+        group = getgrgid(stat_info.st_gid).gr_name if options['show_group'] else ''
+        created = format_date(stat_info.st_ctime) if options['show_created'] else ''
+        modified = format_date(stat_info.st_mtime) if options['show_modified'] else ''
+        
+        # Check if the file is executable, not a directory, and set color accordingly
+        is_executable = os.access(entry_path, os.X_OK) and not is_dir
+        entry_color = 'green' if is_executable else ('blue' if is_dir else 'default')
+        entry_display = f"'{entry}'" if ' ' in entry else entry
+        entry_display = f"[{entry_color}]{get_entry_icon(entry, is_dir)}{entry_display}"
+
+        # Construct and return the row data, including a 'name' key for sorting by name
+        return {
+            'permissions': permissions,
+            'links': links,
+            'legible_size': size,
+            'owner': owner,
+            'group': group,
+            'created': created,
+            'modified': modified,
+            'display_name': entry_display,
+            'name': entry,
+            'size': directory_sizes.get(entry, 0) if is_dir and options['show_folder_size'] else stat_info.st_size,
+            'is_dir': is_dir
+        }
+    except FileNotFoundError:
+        # Handle missing files gracefully
+        return None
+
+def list_directory(
+    path='.',
+    table_format=False,
+    show_header=False,
+    sort_by='name',
+    show_created=False,
+    show_modified=False,
+    show_all=False,
+    show_links=False,
+    show_size=False,
+    show_permissions=False,
+    show_owner=False,
+    show_group=False,
+    show_folder_size=False,
+    reverse_sorting=False,
+    quiet=False,
+):
+    entries = [entry for entry in os.listdir(path) if show_all or not entry.startswith('.')]
+
+    options = {
+        'show_created': show_created,
+        'show_modified': show_modified,
+        'show_all': show_all,
+        'show_links': show_links,
+        'show_size': show_size,
+        'show_permissions': show_permissions,
+        'show_owner': show_owner,
+        'show_group': show_group,
+        'show_folder_size': show_folder_size
+    }
+
+    if table_format:
+        table = Table(show_header=show_header, header_style="bold magenta", box=None, show_edge=False, show_lines=False)
+        columns = []
+
+        if show_permissions:
+            columns.append("Permissions")
+        if show_links:
+            columns.append("Links")
+        if show_size:
+            columns.append("Size")
+        if show_owner:
+            columns.append("Owner")
+        if show_group:
+            columns.append("Group")
+        if show_created:
+            columns.append("Created")
+        if show_modified:
+            columns.append("Modified")
+        columns.append("Name")
+            
+        for column in columns:
+            table.add_column(column, justify="right" if column in ["Size", "Links"] else "left")
+
+        dir_entries = [entry for entry in entries if os.path.isdir(os.path.join(path, entry))]
+        directory_sizes = calculate_directory_sizes(dir_entries, path, show_all, quiet) if show_folder_size else {}
+
+        rows_data = []
+        for entry in entries:
+            row_data = get_row_data(entry, path, options, directory_sizes=directory_sizes)
+            if row_data:
+                rows_data.append(row_data)
+        
+        def custom_sort(row):
+            if sort_by == 'size':
+                if show_folder_size:
+                    return (-row['size'])
+                else:
+                    return (not row['is_dir'] if reverse_sorting else row['is_dir'], -row['size'])
+            return row[sort_by].lower()
+
+        rows_data.sort(key=custom_sort, reverse=reverse_sorting)
+
+        for row_data in rows_data:
+            data_to_add = []
+            if show_permissions:
+                data_to_add.append(row_data['permissions'])
+            if show_links:
+                data_to_add.append(row_data['links'])
+            if show_size:
+                data_to_add.append(row_data['legible_size'])
+            if show_owner:
+                data_to_add.append(row_data['owner'])
+            if show_group:
+                data_to_add.append(row_data['group'])
+            if show_created:
+                data_to_add.append(row_data['created'])
+            if show_modified:
+                data_to_add.append(row_data['modified'])
+            data_to_add.append(row_data['display_name'])
+            table.add_row(*data_to_add)
+
+        console.print(table)
+    else:
+        for entry in entries:
+            entry_path = os.path.join(path, entry)
+            is_dir = os.path.isdir(entry_path)
+            is_executable = os.access(entry_path, os.X_OK) and not is_dir
+            entry_color = 'green' if is_executable else ('blue' if is_dir else 'default')
+            entry_display = f"'{entry}'" if ' ' in entry else entry
+            entry_display = f"[{entry_color}]{get_entry_icon(entry, is_dir)}{entry_display}"
+            console.print(f"[{entry_color}]{entry_display}", end='  ')
+        console.print()
+
+parser = argparse.ArgumentParser(description="List files and directories with options for sorting, showing hidden ones, and coloring.")
+
+parser.add_argument('path', nargs='?', default='.', help='Path for listing (default is the current directory).')
+parser.add_argument('-t', '--table', action='store_true', help='Use a table listing format.')
+parser.add_argument('-H', '--show-header', action='store_true', help='Show table header.')
+parser.add_argument('-a', '--all', action='store_true', help='Include hidden files and directories.')
+parser.add_argument('-s', '--sort-by', choices=['name', 'size', 'created', 'modified'], default='name', help='Sort by name (default), size, creation date, or modification date.')
+parser.add_argument('-c', '--show-created', action='store_true', help='Show creation date of files and directories.')
+parser.add_argument('-m', '--show-modified', action='store_true', help='Show last modification date of files and directories.')
+parser.add_argument('-n', '--show-links', action='store_true', help='Show number of links to files and directories.')
+parser.add_argument('-b', '--show-size', action='store_true', help='Show size of files and directories.')
+parser.add_argument('-p', '--show-permissions', action='store_true', help='Show permissions of files and directories.')
+parser.add_argument('-u', '--show-owner', action='store_true', help='Show owner of files and directories.')
+parser.add_argument('-g', '--show-group', action='store_true', help='Show group of files and directories.')
+parser.add_argument('-x', '--show-folder-size', action='store_true', help='Show total size of directories.')
+parser.add_argument('-r', '--reverse-sorting', action='store_true', help='Reverse the sorting order.')
+parser.add_argument('-q', '--quiet', action='store_true', help='Suppress progress bars and other non-essential output')
+
+args = parser.parse_args()
+
+list_directory(
+    path=args.path,
+    table_format=args.table,
+    show_header=args.show_header,
+    sort_by=args.sort_by,
+    show_created=args.show_created,
+    show_modified=args.show_modified,
+    show_all=args.all,
+    show_links=args.show_links,
+    show_size=args.show_size,
+    show_permissions=args.show_permissions,
+    show_owner=args.show_owner,
+    show_group=args.show_group,
+    show_folder_size=args.show_folder_size,
+    reverse_sorting=args.reverse_sorting,
+    quiet=args.quiet,
+)
